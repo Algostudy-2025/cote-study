28094a4662f7f0e5e28b122332ee809db6ca8168
Mingi Cho <81455273+ChoMinGi@users.noreply.github.com>
2025-07-31 16:59:40 +0900

    Update 389479.java

--- WEEK 6/chomingi/389479.java
+++ WEEK 6/chomingi/389479.java
@@ -1,10 +1,14 @@
 import java.util.*;
-class Solution {
-  
-int active = 0;
-Queue<Integer> expireQueue = new LinkedList<>();
 
-for (int i = 0; i < players.length; i++) {
+class Solution {
+
+  static Deque<Integer> deque;
+  public int solution(int[] players, int m, int k) {
+  
+  int active = 0;
+  Queue<Integer> expireQueue = new LinkedList<>();
+  
+  for (int i = 0; i < players.length; i++) {
     while (!expireQueue.isEmpty() && expireQueue.peek() == i) {
         expireQueue.poll();
         active--;
@@ -16,5 +20,5 @@ for (int i = 0; i < players.length; i++) {
         expireQueue.add(i + k);
         active++;
     }
-}
+  }
 }
