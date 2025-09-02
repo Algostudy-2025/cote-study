e89d0d377a188a810bdbc0470b94668e38591e69
eg1mb <eg1mb@naver.com>
2025-09-03 01:29:11 +0900

    [W10] 김규민_주석추가

--- WEEK 10/gyumin/P42583.java
+++ WEEK 10/gyumin/P42583.java
@@ -3,6 +3,7 @@ import java.util.*;
 class P42583 {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         
+        // 다리 역할을 할 큐
         Deque<Integer> bridge = new LinkedList<>();
         int answer = 0;
         int total_weights = 0;
@@ -13,6 +14,7 @@ class P42583 {
         while(counter < truck_amount) {
             answer++;
             
+            // 내릴 트럭이 있으면 먼저 내리기
             if(bridge.size() >= bridge_length) {
                 int temp = bridge.poll();
                 total_weights -= temp;
@@ -20,12 +22,14 @@ class P42583 {
             	    counter++;
                 }
             }
-            
+
+            // 조건에 맞춰 트럭 올리기
             if(idx < truck_amount && total_weights + truck_weights[idx] <= weight) {
                 bridge.offer(truck_weights[idx]);
                 total_weights += truck_weights[idx];
                 idx++;
             } else {
+                // 트럭을 못 올리는 경우 빈칸 처리
                 bridge.offer(0);
             }
         }
