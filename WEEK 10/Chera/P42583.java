3279cd78ee7dfd1c1a4081482a9b45c8968f0c20
DESKTOP-RBNO6LS\SSAFY <chera1009@naver.com>
2025-09-03 19:17:22 +0900

    [W10] 이체라

--- WEEK 10/Chera/P42583.java
+++ WEEK 10/Chera/P42583.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         Queue<Integer> bridge = new ArrayDeque<>(); // 다리 현재 올라간 무게들 (칸)
@@ -7,7 +8,7 @@ class Solution {
         int sum = 0;            // 다리 위 총 무게
         int idx = 0;            // 대기 트럭 인덱스
 
-        // 처음엔 다리가 비어 있으므로 0으로 채움
+        // 처음엔 다리가 비어 있으므로 0으로 채움 !!
         for (int i = 0; i < bridge_length; i++) bridge.add(0);
 
         // 대기 트럭이 남아 있거나, 다리 위에 아직 무게가 있으면 계속 진행
